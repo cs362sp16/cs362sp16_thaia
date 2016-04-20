4a0c2ee6ea6b26ee2fd281db5b601d636fbce7ae
Aileen <dapper.whale@gmail.com>
2016-04-19 20:36:22 -0700

    Introduce bugs

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -698,7 +698,7 @@ int play_minion(int choice1, int choice2, struct gameState *state, int handPos){
   int currentPlayer = whoseTurn(state);  
     
   //+1 action
-  state->numActions++;
+  //state->numActions++;
 			
   //discard card from hand
   discardCard(handPos, currentPlayer, state, 0);
@@ -745,14 +745,14 @@ int play_steward(int choice1, int choice2, int choice3,
   if (choice1 == 1){
 	  //+2 cards
 	  drawCard(currentPlayer, state);
-	  drawCard(currentPlayer, state);
+	  //drawCard(currentPlayer, state);
 	}else if (choice1 == 2){
 	  //+2 coins
 	  state->coins = state->coins + 2;
 	}else{
 	  //trash 2 cards in hand
 	  discardCard(choice2, currentPlayer, state, 1);
-	  discardCard(choice3, currentPlayer, state, 1);
+	  //discardCard(choice3, currentPlayer, state, 1);
 	}
   //discard card from hand
   discardCard(handPos, currentPlayer, state, 0);
