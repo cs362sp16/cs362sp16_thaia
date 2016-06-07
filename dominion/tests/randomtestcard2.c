3e6686d8f9fc2f262e9238b42c8ec8f8ad390e50
Aileen <dapper.whale@gmail.com>
2016-06-06 17:34:56 -0700

    I'm an idiot that forgot to re-initialize the game

--- dominion/tests/randomtestcard2.c
+++ dominion/tests/randomtestcard2.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include "../dominion.h"
 #include "../rngs.h"
+#include "../interface.h"
 
 // Random Test Smithy
 
@@ -22,21 +23,26 @@ void checkasserts(){
   }
 }
 
-int runthru(struct gameState *g, int handSz, int deckSz, int currcoins,
-            int nDiscard, int who){
+int runthru(struct gameState *g, int handSz, int who){
   int val;
 
   val = g->handCount[who];
+  printf("player: %d\n", who);
+  printf("old hand count: %d\n", handSz);
+  printf("new hand count: %d\n", val);
   assertTF(val != handSz, "Number of cards in hand has changed\n");
-  val = g->deckCount[who];
-  assertTF(val != deckSz, "Number of cards in deck has changed\n");
-  val = g->coins;
-  assertTF(val != currcoins, "Number of coins in hand has changed\n");
-  val = g->discardCount[who];
-  assertTF(val != nDiscard, "Card(s) have been discarded\n");
+  //val = g->deckCount[who];
+  //assertTF(val != deckSz, "Number of cards in deck has changed\n");
+  //val = g->coins;
+  //assertTF(val != currcoins, "Number of coins in hand has changed\n");
+  //val = g->discardCount[who];
+  //assertTF(val != nDiscard, "Card(s) have been discarded\n");
 
   if (failed){
     printf("FAILURE FOUND\n\n");
+  }
+  else{
+    printf("TEST PASSED\n\n");
   }
 
   failed = 0;
@@ -44,8 +50,7 @@ int runthru(struct gameState *g, int handSz, int deckSz, int currcoins,
 }
 
 int main(int argc, char *argv[]){
-  int i, j, numplayers, randSeed, runt, volte;
-  int deckSz, handSz, nDiscard, currcoins;
+  int i, j, numplayers, randSeed, runt, volte, handSz;
   int k[10] = {smithy, adventurer, gardens, embargo, cutpurse, mine, ambassador,
                outpost, baron, tribute};
   int choice[4];
@@ -61,31 +66,43 @@ int main(int argc, char *argv[]){
     randSeed = rand();
   }
 
-  numplayers = rand() % 2 + 2;
-  initializeGame(numplayers, k, randSeed, &g);
+  //numplayers = rand() % 3 + 2;
+  //initializeGame(numplayers, k, randSeed, &g);
 
   volte = rand() % 1000;
-  printf("Tests run: %d\n", volte);
+  printf("Tests run: %d\n", (volte));
 
   for (runt = 0; runt < volte; runt++){
-    for (i = 0; i < numplayers; i++){
-      g.deckCount[i] = rand() % MAX_DECK;
-      g.handCount[i] = rand() % MAX_HAND;
-      g.discardCount[i] = rand() % MAX_HAND;
-      for (j = 0; j < 3; j++){
-        choice[i] = rand() % 2 + 1;
-      }
-      //r = cardEffect(smithy, choice[0], choice[1], choice[2], &g, 0, 0);
-      //assertTF(r == 0, "Smithy played successfully\n");
-      g.hand[0][0] = smithy;
-      deckSz = g.deckCount[i];
-      handSz = g.handCount[i];
-      nDiscard = g.discardCount[i];
-      currcoins = g.coins;
-  
-      playCard(0, choice[0], choice[1], choice[2], &g);
-      runthru(&g, handSz, deckSz, currcoins, nDiscard, i);
-    }
+      numplayers = rand() % 3 + 2;
+      initializeGame(numplayers, k, randSeed, &g);
+    //for (i = 0; i < numplayers; i++){
+      //i = 0;
+      g.handCount[0] = rand() % 10; // gen random hand count for player i
+      /*for (j = 0; j < 3; j++){
+        choice[j] = rand() % 3 + 1;
+        choice[j] = 0;
+      }*/
+      g.hand[0][0] = smithy;  // set first card for player i, to be smithy
+      handSz = g.handCount[0];
+      printf("Player's hand before turn\n");
+      printHand(0, &g);
+      printf("Before play: %d\n", handSz);
+ 
+      //playCard(handPos, choice1, choice2, choice3, gameState) 
+      //playCard(i, choice[0], choice[1], choice[2], &g);
+      //if (g.hand[i][0] == smithy){
+      playCard(0, 0, 0, 0, &g);
+        //j = cardEffect(smithy, 0, 0, 0, &g, 0, 0);
+        //assertTF(j == 0, "Smithy played\n");
+      printf("After play: %d\n", g.handCount[0]);
+      runthru(&g, handSz, 0);
+      printf("Player's hand after turn\n");
+      printHand(0, &g);
+      //}
+      //else{
+      //  printf("Fuck everything\n");
+      //}
+    //}
   }
   printf("``smithy`` -- RANDOM TESTING COMPLETE \n");
   checkasserts();
